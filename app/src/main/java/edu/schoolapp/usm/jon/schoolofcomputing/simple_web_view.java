3c9610a6f2f10e9a1a17086acfc3ce411f1649ae
jhare9 <jonathon.hare@eagles.usm.edu>
2015-04-14 15:37:19 -0500

    let the user know the webviews are loading.

--- app/src/main/java/edu/schoolapp/usm/jon/schoolofcomputing/simple_web_view.java
+++ app/src/main/java/edu/schoolapp/usm/jon/schoolofcomputing/simple_web_view.java
@@ -1,6 +1,7 @@
 package edu.schoolapp.usm.jon.schoolofcomputing;
 
 import android.app.ActionBar;
+import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -10,11 +11,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 /**
  * Created by jon on 4/13/2015.
  */
 public class simple_web_view extends Fragment{
+
+    private WebView webview;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,12 +28,18 @@ public class simple_web_view extends Fragment{
         android.support.v7.app.ActionBar ab = abv.getSupportActionBar();
         ab.setTitle(getArguments().getString("department name"));
 
-        WebView webview = (WebView) inflater.inflate(R.layout.simple_web_view,container,false);
+        Toast.makeText(getActivity(),"loading..",Toast.LENGTH_SHORT).show();
+
+        webview = (WebView) inflater.inflate(R.layout.simple_web_view,container,false);
         webview.loadDataWithBaseURL("file:///android_res/drawable/",getArguments().getString("html"), "text/html", "utf-8", null);
         webview.getSettings().setBuiltInZoomControls(true);
         webview.setInitialScale(120);
         webview.setBackgroundColor(Color.BLACK);
         webview.getSettings().setNeedInitialFocus(true);
+
+
+
         return webview;
     }
+
 }
